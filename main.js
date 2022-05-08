925dd50dd8a9a025b14a17cb8da1a7242cee85b5
p0lygun <solankivibhakar82@gmail.com>
2022-05-09 00:22:20 +0530

    import files from p0lygun/p0lygun.github.io

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// window.addEventListener(
+//     'scroll',
+//     (event) => {
+//         let s = (this.scrollY / document.body.clientHeight) * 100;
+//         if (s > 10) {
+//             document.getElementById('navbar').style.backgroundColor = "transparent";
+//         }else {
+//             document.getElementById('navbar').style.backgroundColor = "#17283c";
+//         }
+//     }
+// )
